37f32addec0889ea40f78393de63e62f30b394cd
Benjamin Dow <benjamindow1@gmail.com>
2025-01-22 05:27:02 -0500

    alias

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -15,4 +15,9 @@ resource "aws_route53_record" "env_record" {
   name    = "${var.environment}.${data.aws_route53_zone.rootdns.name}"
   type    = "A"
   zone_id = data.aws_route53_zone.rootdns.zone_id
+  alias {
+    evaluate_target_health = true
+    name                   = aws_s3_bucket.website_bucket.website_endpoint
+    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
+  }
 }
