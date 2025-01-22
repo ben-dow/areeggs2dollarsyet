d0d6f5090fc73307100e6372d1258bc7118d57b0
Benjamin Dow <benjamindow1@gmail.com>
2025-01-22 06:55:45 -0500

    private

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,27 +9,9 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "access_block" {
-  bucket = aws_s3_bucket.website_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = aws_s3_bucket.website_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.access_block]
-}
-
 resource "aws_s3_bucket_acl" "bucket-acl" {
   bucket = aws_s3_bucket.website_bucket.id
-  acl = "public-read"
-  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+  acl = "private"
 }
 
 resource "aws_s3_bucket_policy" "bucket-policy" {
@@ -51,8 +33,6 @@ data "aws_iam_policy_document" "iam-policy-1" {
       identifiers = ["*"]
     }
   }
-
-  depends_on = [aws_s3_bucket_public_access_block.access_block]
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
