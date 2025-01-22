fa840877a72303652f3a91688db2f43bf4598e7b
Benjamin Dow <benjamindow1@gmail.com>
2025-01-22 05:07:37 -0500

    perms

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,40 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "areeggs2dollarsyet-${var.environment}"
 }
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_acl" "bucket-acl" {
   bucket = aws_s3_bucket.website_bucket.id
   acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = data.aws_iam_policy_document.iam-policy-1.json
+}
+data "aws_iam_policy_document" "iam-policy-1" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}",
+      "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+    ]
+    actions = ["S3:GetObject"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
