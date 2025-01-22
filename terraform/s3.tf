2f089b5733494415755464d8fe4044c395e580dd
Benjamin Dow <benjamindow1@gmail.com>
2025-01-21 20:41:04 -0500

    trigger

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket" "website_bucket" {
   bucket = "areeggs2dollarsyet-${var.environment}"
 }
 
+
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website_bucket.id
   index_document {
