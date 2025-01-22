b86daf4a72c5bade761fc565c5d9b57ae1fbe681
Benjamin Dow <benjamindow1@gmail.com>
2025-01-22 06:20:45 -0500

    cert

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -37,7 +37,7 @@ resource "aws_cloudfront_distribution" "website" {
     }
   }
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.cert.arn
   }
 
 }
