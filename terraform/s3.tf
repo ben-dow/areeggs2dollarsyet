fdf49f9c39ba3ca5b6948fdb7f9a834ba3a540b0
Benjamin Dow <benjamindow1@gmail.com>
2025-01-21 21:02:41 -0500

    dist

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -16,6 +16,6 @@ resource "aws_s3_object" "dist" {
   for_each = fileset("../dist/", "*")
   bucket = aws_s3_bucket.website_bucket.id
   key = each.value
-  source = "dist"
+  source = "../dist"
   etag = filemd5("../dist/${each.value}")
 }
