d860e6d718f77f34ab90723d2160f26d5f33e6de
Benjamin Dow <benjamindow1@gmail.com>
2025-01-26 14:41:40 -0500

    prod records

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -12,3 +12,15 @@ resource "aws_route53_record" "env_record" {
     zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
   }
 }
+
+resource "aws_route53_record" "env_record" {
+  count = var.environment == "prod" ? 1 : 0
+  name    = data.aws_route53_zone.rootdns.name
+  type    = "A"
+  zone_id = data.aws_route53_zone.rootdns.zone_id
+  alias {
+    evaluate_target_health = true
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+  }
+}
