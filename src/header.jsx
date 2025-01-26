64af8e261d5675971d86d69163968de3d021c134
Benjamin Dow <benjamindow1@gmail.com>
2025-01-26 15:32:20 -0500

    footer

--- src/header.jsx
+++ src/header.jsx
@@ -17,11 +17,15 @@ function Header() {
             </div>
             <div className={"w-full shadow-lg bg-slate-200 flex flex-col gap-2 justify-center p-5"}>
                 <div className={"text-center text-slate-700 flex justify-center"}>
-                    <p className={"text-wrap w-5xl text-sm"}>Below is a timeline of actions taken by the trump administration
+                    <p className={"text-wrap w-5xl text-sm"}>Below is a timeline of actions taken by the trump
+                        administration
                         since taking office on January 20th 2025.
-                        Each action is rated on a scale of 1 - 10 based on how little (1) or how much (10) the action
-                        seems to be expected
-                        to address inflationary effects around egg prices and other commodities.
+                        <br/>
+                        <br/>
+                        Each item is rated from 1 - 10 on our Egg Meter regarding how they seem to affect pricing on Eggs (and similar items) for consumers
+                        <br/>
+                        The lower the number, the more likely it seems the action may hurt consumers, the higher the more likely it will help. Items rated a 5
+                        do not seem like they will have any effect.
                     </p>
                 </div>
                 <EggScale/>
@@ -33,7 +37,7 @@ function Header() {
 
 function EggScale() {
     const rows = [];
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         rows.push(<div className={"text-center"}>
                     <EggMeter key={i} value={i}/>
                     {i}
